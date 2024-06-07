b22dadf0fadb5cb094f7df15bd0a271ea497d03b
Wahyu2021 <wahyuwn256@gmail.com>
2024-06-07 23:46:40 +0700

    done

--- authentication.js
+++ authentication.js
@@ -1,42 +1,41 @@
-const submit = document.querySelector('.submit');
-const daftar = document.querySelector('#signUp');
-const form = document.querySelector('#signInForm');
-const username = document.querySelector('#username');
-const password = document.querySelector('#password');
-
+const submit = document.querySelector(".submit");
+const daftar = document.querySelector("#signUp");
+const form = document.querySelector("#signInForm");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
 // masuk ke page sign up
-daftar.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('.loader-container').style.visibility = 'visible';
-    setTimeout(() => {
-        window.location.href = 'create-account-page/sign-up.html'
-        document.querySelector('.loader-container').style.visibility = 'hidden';
-    },1000)
-})
-
+daftar.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".loader-container").style.visibility = "visible";
+  setTimeout(() => {
+    window.location.href = "create-account-page/sign-up.html";
+    document.querySelector(".loader-container").style.visibility = "hidden";
+  }, 1000);
+});
 
 // sign in
-submit.addEventListener('click',async (e) => {
-    e.preventDefault();
-    
-    if((localStorage.getItem('username') === username.value.trim() && localStorage.getItem('password') === password.value.trim()) || (username.value.trim() === 'admin' && password.value.trim() === '123')){
-        await Swal.fire({
-            icon: "success",
-            title: "Successful Login",
-        });
-        document.querySelector('.loader-container').style.visibility = 'visible';
-        setTimeout(() => {
-            document.querySelector('.loader-container').style.visibility = 'hidden';
-            window.location.href = 'home-page/home.html'
-        },1000)
-    } else{
-        Swal.fire({
-            icon: "error",
-            title: "Invalid username or password"
-        });
-    }
-})
+submit.addEventListener("click", async (e) => {
+  e.preventDefault();
 
-
-
+  if (
+    (localStorage.getItem("username") === username.value.trim() &&
+      localStorage.getItem("password") === password.value.trim()) ||
+    (username.value.trim() === "admin" && password.value.trim() === "123")
+  ) {
+    await Swal.fire({
+      icon: "success",
+      title: "Successful Login",
+    });
+    document.querySelector(".loader-container").style.visibility = "visible";
+    setTimeout(() => {
+      document.querySelector(".loader-container").style.visibility = "hidden";
+      window.location.href = "home-page/home.html";
+    }, 1000);
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Invalid username or password",
+    });
+  }
+});
--- home-page/homeApp.js
+++ home-page/homeApp.js
@@ -1,86 +1,85 @@
-const navbar = document.querySelector('.navbar');
-const navLinks = document.querySelectorAll('.nav-link');
-const navBorder = document.querySelector('.nav-border');
-const btnPesan = document.querySelector('#btnPesan');
-const penerbanganGroup = document.querySelector('.penerbangan-group');
-const hotelGroup = document.querySelector('.hotel-group');
-const busGroup = document.querySelector('.bus-group');
-const titlePopulers = document.querySelectorAll('.title-populer');
+const navbar = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".nav-link");
+const navBorder = document.querySelector(".nav-border");
+const btnPesan = document.querySelector("#btnPesan");
+const penerbanganGroup = document.querySelector(".penerbangan-group");
+const hotelGroup = document.querySelector(".hotel-group");
+const busGroup = document.querySelector(".bus-group");
+const titlePopulers = document.querySelectorAll(".title-populer");
 const threshold = 50;
 const screenWidth = window.innerWidth;
 
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   let scrollPosition = window.scrollY;
-  if(screenWidth <= 992){
-    navbar.style.backgroundColor = '#86B6F6';
-  } else{
+  if (screenWidth <= 992) {
+    navbar.style.backgroundColor = "#86B6F6";
+  } else {
     if (scrollPosition >= threshold) {
-        navbar.style.backgroundColor = '#86B6F6';
-        navBorder.style.borderLeft = "2px solid #252525a8"
-        navLinks.forEach((navLink) =>{
-          navLink.style.color = "black";
-        })
+      navbar.style.backgroundColor = "#86B6F6";
+      navBorder.style.borderLeft = "2px solid #252525a8";
+      navLinks.forEach((navLink) => {
+        navLink.style.color = "black";
+      });
     } else {
-        navbar.style.backgroundColor = 'transparent';
-        navBorder.style.borderLeft = "2px solid #b9b8b893"
-        navLinks.forEach((navLink) =>{
-          navLink.style.color = "white";
-        })
+      navbar.style.backgroundColor = "transparent";
+      navBorder.style.borderLeft = "2px solid #b9b8b893";
+      navLinks.forEach((navLink) => {
+        navLink.style.color = "white";
+      });
     }
   }
-  
 });
 
-btnPesan.addEventListener('click', () => {
+btnPesan.addEventListener("click", () => {
   Swal.fire({
     title: "Are you sure?",
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Yes!"
+    confirmButtonText: "Yes!",
   }).then((result) => {
     if (result.isConfirmed) {
       Swal.fire({
         title: "Thank you!",
-        icon: "success"
+        icon: "success",
       });
     }
   });
-})
+});
 
 const removeActiveClass = () => {
   titlePopulers.forEach((titlePopuler) => {
-    titlePopuler.classList.remove('this-menu');
+    titlePopuler.classList.remove("this-menu");
   });
-}
+};
 
 // penerbangan
-document.querySelector('#penerbangan').addEventListener('click', () => {
+document.querySelector("#penerbangan").addEventListener("click", () => {
   // display
   removeActiveClass();
-  penerbanganGroup.style.display = 'flex';
-  hotelGroup.style.display = 'none';
-  busGroup.style.display = 'none';
+  penerbanganGroup.style.display = "flex";
+  hotelGroup.style.display = "none";
+  busGroup.style.display = "none";
 
   // change menu
-  document.querySelector('#penerbangan').classList.add('this-menu')
-})
+  document.querySelector("#penerbangan").classList.add("this-menu");
+});
 
 // hotel
-document.querySelector('#hotel').addEventListener('click', () => {
+document.querySelector("#hotel").addEventListener("click", () => {
   removeActiveClass();
-  penerbanganGroup.style.display = 'none';
-  hotelGroup.style.display = 'flex';
-  busGroup.style.display = 'none';
-  document.querySelector('#hotel').classList.add('this-menu')
-})
+  penerbanganGroup.style.display = "none";
+  hotelGroup.style.display = "flex";
+  busGroup.style.display = "none";
+  document.querySelector("#hotel").classList.add("this-menu");
+});
 
 // bus
-document.querySelector('#bus').addEventListener('click', () => {
+document.querySelector("#bus").addEventListener("click", () => {
   removeActiveClass();
-  penerbanganGroup.style.display = 'none';
-  hotelGroup.style.display = 'none';
-  busGroup.style.display = 'flex';
-  document.querySelector('#bus').classList.add('this-menu')
-})
+  penerbanganGroup.style.display = "none";
+  hotelGroup.style.display = "none";
+  busGroup.style.display = "flex";
+  document.querySelector("#bus").classList.add("this-menu");
+});
